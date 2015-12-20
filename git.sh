91d7df588863cc56e1f8b2772aaa8f89f2438504
weixingsun <weixing.sun@gmail.com>
2015-12-20 21:15:52 +0000

    build from ubuntu1510

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# lightweight java-profiler" >> README.md
+#git init
+#git add README.md
+git commit -m "first commit"
+#git remote add origin https://github.com/weixingsun/java-profiler.git
+git push -u origin master
